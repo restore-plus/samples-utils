28f85513b04ff13e1a890c7af8786ccf3eb483cd
Felipe Carlos <efelipecarlos@gmail.com>
2025-09-16 01:02:17 +0000

    samples: rename data

--- analysis/merge-samples-from-analysis.R
+++ analysis/merge-samples-from-analysis.R
@@ -5,4 +5,4 @@ base_output_dir <- fs::path("data/derived/samples/")
 samples_2010 <- fs::dir_ls(base_output_dir / "samples-from-evaluation") |>
                   purrr::map_dfr(readRDS)
 
-saveRDS(samples_2010, base_output_dir / "samples-from-evaluation.rds")
+saveRDS(samples_2010, base_output_dir / "samples-eco3-2010.rds")
